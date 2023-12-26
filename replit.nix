9b9b10e970e52316d39d7b0cd81e0b7f4d46c8dc
Replit user <>
2023-12-26 07:56:57 +0000

    處理兩個 Brython 頁面的衝突以及 .replit 與 replit.nix 的合併錯誤

--- replit.nix
+++ replit.nix
@@ -1,14 +1,5 @@
 {pkgs}: {
   deps = [
     pkgs.libev
-<<<<<<< HEAD
   ];
-=======
-   ];
-  env = {
-    PYTHON_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-      pkgs.libev
-    ];
-  };
->>>>>>> 0bfbc06e4a10ee6641c409785ee9b8b357b20cdf
 }
